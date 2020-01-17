493b432c7f8661ef852480764301cdcb6f788ecb
Lucinda Sisk <lucinda.sisk@gmail.com>
2020-01-16 23:59:23 -0500

    added script to run Rmd

---
+++ sbatch_CMI_ABCD_JIVE_Run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#SBATCH --job-name=CMI_ABCD_JIVE_analysis
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=36:00:00
+#SBATCH --partition=verylong
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+
+date=$(date +%m.%d.%Y)
+
+R -e /gpfs/milgram/project/gee_dylan/lms233/CMI_ABCD/CMI_ABCD_Subtyping/CMI_ABCD_JIVE_test.Rmd
