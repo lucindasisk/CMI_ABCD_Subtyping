1f05d76c037c1b6f90a074911587a6db637f5d39
Lucinda Sisk <lucinda.sisk@gmail.com>
2020-01-18 09:03:04 -0500

    update

--- sbatch_CMI_ABCD_JIVE.sh
+++ sbatch_CMI_ABCD_JIVE.sh
@@ -12,5 +12,6 @@
 
 date=$(date +%m.%d.%Y)
 
+
 R -e "rmarkdown::render('/gpfs/milgram/project/gee_dylan/lms233/CMI_ABCD/CMI_ABCD_Subtyping/CMI_ABCD_JIVE_Cluster.Rmd',
-output_file='CMI_ABCD_JIVE_Report.pdf')"
+output_file='$1')"
