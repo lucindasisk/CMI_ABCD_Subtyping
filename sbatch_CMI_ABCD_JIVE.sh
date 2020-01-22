bced9d5e00d9f9fc98393072bdf9dddbb583318f
Lucinda Sisk <lucinda.sisk@gmail.com>
2020-01-22 11:05:17 -0500

    update filename

--- sbatch_CMI_ABCD_JIVE.sh
+++ sbatch_CMI_ABCD_JIVE.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=10G
-#SBATCH --time=36:00:00
-#SBATCH --partition=verylong
+#SBATCH --time=10:00:00
+#SBATCH --partition=long
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lucinda.sisk@yale.edu
 
@@ -14,4 +14,4 @@ date=$(date +%m.%d.%Y)
 
 
 R -e "rmarkdown::render('/gpfs/milgram/project/gee_dylan/lms233/CMI_ABCD/CMI_ABCD_Subtyping/CMI_ABCD_JIVE_Cluster.Rmd',
-output_file='$1')"
+output_file='CMI_ABCD_JIVE_Report_'$date'.pdf')"
