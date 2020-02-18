de52bb4d1e30861873227bd449eaac0c76f5d321
Lucinda Sisk <lucinda.sisk@gmail.com>
2020-02-17 20:14:04 -0500

    add new version with separated brain vars and new pheno vars by LS

---
+++ sbatch_CMI_ABCD_JIVE_Run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#SBATCH --job-name=CMI_ABCD_JIVE_analysis
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=36:00:00
+#SBATCH --partition=verylong
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lucinda.sisk@yale.edu
+
+date=$(date +%m.%d.%Y)
+
+Rscript Jive_Script_Cluster.R
